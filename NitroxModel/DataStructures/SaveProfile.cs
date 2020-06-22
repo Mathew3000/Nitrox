47bc80491650825e19dd5a8b199807f32b7c0aff
Mathew96 <darklord96@gmx.de>
2020-06-23 00:21:42 +0200

    Added filesystem operations and refactored code

--- NitroxModel/DataStructures/SaveProfile.cs
+++ NitroxModel/DataStructures/SaveProfile.cs
@@ -8,7 +8,6 @@ namespace NitroxModel.DataStructures
 {
     class SaveProfile
     {
-
         #region Constants
         /// <summary>
         /// The foldername where Nitrox SaveProfiles are stored
@@ -22,50 +21,78 @@ namespace NitroxModel.DataStructures
         public static List<SaveProfile> Profiles { get; private set; }
         #endregion
 
-
         #region Properties
         public string ProfileName { get; private set; }
         public string ProfilePath { get; private set; }
 
+        /// <summary>
+        /// The path to the profiles "world" folder
+        /// </summary>
+        public string WorldPath
+        {
+            get
+            {
+                return Path.Combine(ProfilePath, "world");
+            }
+        }
+
+        /// <summary>
+        /// The path to the profiles "dev" folder
+        /// </summary>
+        public string DevPath
+        {
+            get
+            {
+                return Path.Combine(ProfilePath, "dev");
+            }
+        }
+
+        /// <summary>
+        /// The path to the servers.list file
+        /// </summary>
+        public string ServerListPath
+        {
+            get
+            {
+                return Path.Combine(ProfilePath, "servers.lst");
+            }
+        }
+
+        /// <summary>
+        /// The profiles serverList
+        /// </summary>
         public List<string> ServerList { get; private set; }
         #endregion
 
         #region Constructor
+        /// <summary>
+        /// Creates a new SaveProfile with given name or loads if existent
+        /// </summary>
+        /// <param name="name"></param>
         public SaveProfile(string name)
         {
             ProfileName = name;
             ServerList = new List<string>();
 
-            // Create Folderstructure
             string ProfilePath = Path.Combine(SettingsPath, ProfileFolder, ProfileName);
-            Directory.CreateDirectory(ProfilePath);
-            Directory.CreateDirectory(Path.Combine(ProfilePath, "world"));
-            Directory.CreateDirectory(Path.Combine(ProfilePath, "dev"));
-        }
-
-        public SaveProfile(string name, string path)
-        {
-            ProfileName = name;
-            ServerList = new List<string>();
-
-            // PARTY
-            // Add clean reading of servers
-            ServerList = File.ReadAllLines(Path.Combine(path, "servers"));
+            if (!Directory.Exists(ProfilePath))
+            {
+                Directory.CreateDirectory(ProfilePath);
+                Directory.CreateDirectory(Path.Combine(ProfilePath, "world"));
+                Directory.CreateDirectory(Path.Combine(ProfilePath, "dev"));
+            }
+            else
+            {
+                LoadServerList();
+            }
         }
         #endregion
 
-
         #region PublicMember
-        public string GetWorldPath()
-        {
-            return Path.Combine(ProfilePath, "world");
-        }
-
-        public string GetDevPath()
-        {
-            return Path.Combine(ProfilePath, "dev");
-        }
-        
+        /// <summary>
+        /// Adds a server ip to the profiles server list
+        /// </summary>
+        /// <param name="serverIp"></param>
         public void AddServer(string serverIp)
         {
             if(!ServerList.Contains(serverIp))
@@ -74,6 +101,9 @@ namespace NitroxModel.DataStructures
             }
         }
 
+        /// <summary>
+        /// Removes the profile from disk
+        /// </summary>
         public void Delete()
         {
             Directory.Delete(ProfilePath, true);
@@ -82,29 +112,30 @@ namespace NitroxModel.DataStructures
                 Profiles.Remove(this);
             }
         }
+        
+        /// <summary>
+        /// Saves the current profile settings to disk
+        /// </summary>
+        public void SaveToDisk()
+        {
+            SaveServerList();
+        }
         #endregion
 
         #region PublicStatics
-        public static SaveProfile GetOrCreate(string name)
-        {
-            if (Profiles == null)
-            {
-                GetProfiles();
-            }
-
-            SaveProfile found = Profiles.Find(x => x.ProfileName == name);
-            
-            if (found != null)
-                return found;
-            else
-                return new SaveProfile(name);
-        }
-
+        /// <summary>
+        /// Returns the "default" profile
+        /// </summary>
+        /// <returns></returns>
         public static SaveProfile GetDefaultProfile()
         {
-            return GetOrCreate("default");
+            return SaveProfile("default");
         }
         
+        /// <summary>
+        /// Gets a list of all existing Profiles
+        /// </summary>
+        /// <returns></returns>
         public static List<SaveProfile> GetProfiles()
         {
             Profiles = new List<SaveProfile>();
@@ -112,10 +143,62 @@ namespace NitroxModel.DataStructures
             // List all directories in Profiles folder
             foreach (string profile in Directory.EnumerateDirectories(Path.Combine(SettingsPath, ProfileFolder)))
             {
-                Profiles.Add(new SaveProfile(profile, Path.Combine(SettingsPath, ProfileFolder, profile)));
+                SaveProfile tmp = new SaveProfile(profile, Path.Combine(SettingsPath, ProfileFolder, profile));
+
+                if (tmp.SanityCheck)
+                {
+                    Profiles.Add(tmp);
+                }
             }
             return Profiles;
         }
+        #endregion
+
+        #region PrivateMember
+
+        #region Save
+        private void SaveServerList()
+        {
+            if (File.Exists(ServerListPath))
+            {
+                try
+                {
+                    File.Delete(ServerListPath);
+                }
+                catch
+                {
+                    return;
+                }
+            }
+
+            File.WriteAllLines(ServerListPath, ServerList.ToArray());
+        }
+        #endregion
+
+        #region Load
+        private void LoadServerList()
+        {
+            if (File.Exists(ServerList))
+            {
+                ServerList = File.ReadAllLines(ServerList);
+            }
+            else
+            {
+                ServerList = new List<string>();
+            }
+        }
+
+        private bool SanityCheck()
+        {
+            bool sane = File.Exists(ServerListPath);
+            sane &= Directory.Exists(ProfileFolder);
+            sane &= Directory.Exists(WorldPath);
+            sane &= Directory.Exists(DevPath);
+            
+            return sane;
+        }
+        #endregion
+
         #endregion
     }
 }
